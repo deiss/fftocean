ce184a34131ac54cbee8242cf5dbe53bff8feede
CSWest <olivier.deiss@gmail.com>
2014-12-28 22:50:22 +0100

    comments

--- main.cpp
+++ main.cpp
@@ -1,18 +1,27 @@
+/* DEISS Olivier            */
+/* File : main.cpp          */
+/* Last Update : 12/28/2014 */
+
 #include "Fenetre.hpp"
 #include "Ocean.hpp"
 
+#include <cstdlib>
 #include <ctime>
 #include <iostream>
-#include <cstdlib>
 
+// Ocean(lx, ly, nx, ny, windSpeed,  windAlignment, minWaveSize, A)
+// See Philipps.hpp for more details about these parameters
 Ocean *ocean = new Ocean(170, 350, 64, 128, 60, 1, 0, 0.000002);
 
 int main (int argc, char **argv) {
 	
+    // random generator for gaussian numbers
 	srand(time(NULL));
 	
+    // initial ocean wave height field
 	ocean->generateHeight0();
 	
+    // set up the window
 	Fenetre::init(WIDTH, HEIGHT, "Ocean Simulation", argc, argv);
 	Fenetre::setFPS(35);
 	Fenetre::launch();
