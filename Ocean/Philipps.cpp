f94075c307b8cf33591015334681d9319e4fb026
CSWest <olivier.deiss@gmail.com>
2014-12-28 15:30:00 +0100

    new comments, final version

--- Ocean/Philipps.cpp
+++ Ocean/Philipps.cpp
@@ -1,25 +1,29 @@
+/* DEISS Olivier            */
+/* Class : Philipps         */
+/* Last Update : 28/12/2014 */
+
 #include "Philipps.hpp"
 
 #include <cmath>
 #include <iostream>
 
-Philipps::Philipps(const double vitesseVent, const int alignementVent, const double tailleMinVague, const double A) : _A(A),
-																													  _alignementVent(alignementVent),
-																													  _tailleMinVague(tailleMinVague),
-																													  _vitesseVent(vitesseVent) {
+Philipps::Philipps(const double windSpeed, const int windAlignment, const double minWaveSize, const double A) :
+    _A(A),
+    _windAlignment(windAlignment),
+    _minWaveSize(minWaveSize),
+    _windSpeed(windSpeed) {
 
 }
 
+/* fonctor - the formula is given in J. Tessendorf's paper */
 double Philipps::operator()() {
 	
-	double var;
-	double g(9.81);
+	double var, g(9.81);
 	 
-	double kx((2*M_PI*_x) / _lx);
-	double ky((2*M_PI*_y) / _ly);
+	double kx((2*M_PI*_x) / _lx), ky((2*M_PI*_y) / _ly);
 	double k_sq(kx*kx + ky*ky);
 	
-	double L_sq(pow((_vitesseVent*_vitesseVent) / g, 2));
+	double L_sq(pow((_windSpeed*_windSpeed) / g, 2));
 	
 	_y++;
 	
@@ -28,8 +32,8 @@ double Philipps::operator()() {
 	else {
 	
 		var = _A*exp((-1/(k_sq*L_sq)));
-		var *= exp(-k_sq*pow(_tailleMinVague, 2));
-		var *= pow((kx*kx) / k_sq, _alignementVent);    // vent selon êy
+		var *= exp(-k_sq*pow(_minWaveSize, 2));
+		var *= pow((kx*kx) / k_sq, _windAlignment);    // wind : y direction
 		var /= k_sq*k_sq;
 	
 		return var;
